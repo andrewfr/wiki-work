e5c424d26458d4365d166dd9408821e214bec179
Andrew <andrew.francis@mail.mcgill.ca>
2017-08-08 19:49:14 -0400

    code for writing and testing schedule_info

---
+++ sessions.py
@@ -0,0 +1,26 @@
+def generate_schedule_info(wiki):
+    """
+    args
+       wiki: list of strings representing the wiki schedule
+    """
+    return
+
+
+def get_schedule_info(event):
+    """
+    get schedule information associated with event
+
+    args:
+        event : data structure representing an event
+
+    return:
+        tuple : (name, session_id, session_name)
+
+        name - string
+        session_id - string
+        session_name - string
+    """
+
+    return name, session_id, session_name
+
+        
