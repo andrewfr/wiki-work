e31b1c671a654201c901465a488bff3248368a01
Andrew <andrew.francis@mail.mcgill.ca>
2017-08-08 15:46:33 -0400

    added headers

--- make_csv.py
+++ make_csv.py
@@ -1,14 +1,18 @@
 import codecs
 import csv
 
+headers = ['title', 'description', 'faciliator_array', 'faciliators',
+        'location', 'pathways', 'schedule-block', 'space', 'start', 'end']
+
 def make_csv(file_name, events):
     with codecs.open(file_name, "wb", encoding="utf-8") as fp:
-        writer = csv.writer(fp, delimiter='', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer = csv.writer(fp, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(headers)
 
     return
 
 def main():
-    make_csv()
+    make_csv("test.csv",[])
 
 if  __name__ == "__main__":
     main()
