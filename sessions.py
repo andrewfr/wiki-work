8f06e6e1e5c0e3f42b501dd10dc8176e7b811e85
Andrew <andrew.francis@mail.mcgill.ca>
2017-08-09 03:21:23 -0400

    started generate_session_info()

--- sessions.py
+++ sessions.py
@@ -1,8 +1,30 @@
+import codecs
+import re
+
+session_pattern = re.compile("\s*!\s*(Session\s*\d)")
+schedule_block_info = []
+
+def traverse_schedule(schedule):
+    for line in schedule:
+        yield line
+    raise StopIteration()
+
+
 def generate_schedule_info(wiki):
     """
     args
        wiki: list of strings representing the wiki schedule
     """
+
+    session_blocks = []
+
+    schedule_gen = traverse_schedule(wiki)
+
+    for line in schedule_gen:
+        result = session_pattern.search(line)
+        if result:
+            print result.group(1), line
+
     return
 
 
@@ -23,4 +45,15 @@ def get_schedule_info(event):
 
     return name, session_id, session_name
 
-        
+def open_schedule(file_name):
+    with codecs.open(file_name,encoding="utf-8") as fp:
+        wiki_schedule = [line.strip() for line in fp]
+    return wiki_schedule
+
+def main():
+    wiki = open_schedule("data/friday.wiki")
+    generate_schedule_info(wiki)
+
+
+if __name__ == "__main__":
+    main()
